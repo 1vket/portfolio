3d16b7e56397521a80f9c571ad380bbefdad3894
1vket <ivket3@gmail.com>
2024-09-09 07:50:47 +0000

    Fix network

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def static_files(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5101)
 
